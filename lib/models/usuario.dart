e06ef52c97f9b194536e65e7f5356d33b79335ea
juanpabl090 <juanrona12@gmail.com>
2022-07-27 14:41:34 -0500

    Clase usuario

---
+++ lib/models/usuario.dart
@@ -0,0 +1,14 @@
+class Usuario {
+  bool online;
+  String email;
+  String uid;
+  String nombre;
+
+  Usuario({
+    required this.email,
+    required this.online,
+    required this.uid,
+    required this.nombre,
+  });
+
+}
